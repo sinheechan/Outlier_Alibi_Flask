0589e4d4f390bb19e5a5cda3f2893558e050771c
sinheechan <gmlcks0905@gmail.com>
2024-04-28 13:58:06 +0900

    create

---
+++ app.py
@@ -0,0 +1,78 @@
+import matplotlib
+# matplotlib.use('Agg')  # 백엔드를 Agg로 설정 => Flask 충돌우려로 인한 주석처리
+from flask import Flask, request, render_template, jsonify
+from alibi_detect.utils.saving import load_detector
+from alibi_detect.od import OutlierVAE
+from alibi_detect.utils.visualize import plot_instance_score, plot_feature_outlier_image
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import os
+import time
+
+app = Flask(__name__)
+
+# train.py 결과 모델 로드
+od = load_detector('outlier_detection_model')
+
+# 결과를 저장할 폴더 경로
+results_folder = 'C:/sinheechan.github.io-master/Mlops-outlier-detection_lee/results_image'
+
+# result 폴더가 없는 경우 신규 폴더 생성
+if not os.path.exists(results_folder):
+    os.makedirs(results_folder)
+
+# 파일로 결과를 저장
+def save_result_image(fig, filename_prefix):
+    if fig is None: # 예외처리
+        return None
+    
+    img_path = os.path.join(results_folder, f"{filename_prefix}_{int(time.time())}.png")
+    print("이미지가 저장되었습니다.:", img_path)
+    
+    fig.savefig(img_path)
+    plt.close(fig)
+    
+    return img_path
+
+# 파일을 업로드하면 분석결과 출력
+@app.route('/', methods=['GET', 'POST'])
+
+def home():
+    if request.method == 'POST':
+        
+        # 예외처리1 => 파일을 아예 제출하지 않은 경우
+        if 'file' not in request.files:
+            return render_template('index.html', prediction="제출 된 파일이 없습니다. 파일을 다시 제출해주세요")
+
+        file = request.files['file']
+
+        # 예외처리2 => 파일 업로드 폼에서 파일을 선택하지 않은 경우
+        if file.filename == '':
+            return render_template('index.html', prediction="선택한 파일없음")
+
+        # 정상 파일 제출 시 => 이미지로 변환하는 역할
+        if file:
+            img = Image.open(file.stream).convert('RGB')
+            img = img.resize((64, 64))
+            img = np.asarray(img)
+            img = img.astype(np.float32) / 255. # 정규화
+            img = img.reshape(1, 64, 64, 3)
+
+            # 이미지 데이터 => 이상 탐지 모델에서 재구성
+            x_recon = od.vae(img).numpy()
+
+            # 예측 수행
+            # od.predict : 이미지가 얼마나 정상적인지를 나타내는 값 반환
+            od_preds = od.predict(img, outlier_type='instance', return_feature_score=True, return_instance_score=True)
+
+            # 결과를 시각화, 시각화된 결과를 이미지 파일로 저장
+            instance_score_plot = plot_instance_score(od_preds, [0], ['normal', 'outlier'], od.threshold)
+            instance_score_plot_path = save_result_image(instance_score_plot, 'instance_score_plot')
+
+            feature_outlier_image = plot_feature_outlier_image(od_preds, img, X_recon=x_recon, max_instances=5, outliers_only=False)
+            feature_outlier_image_path = save_result_image(feature_outlier_image, 'feature_outlier_image')
+
+            return render_template('index.html', prediction="Anomaly Score: {:.4f}".format(od_preds['data']['instance_score'][0]), instance_score_plot=instance_score_plot_path, feature_outlier_image=feature_outlier_image_path)
+    else:
+        return render_template('index.html', prediction=None)
---
+++ train.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Dense, Layer, Reshape, InputLayer
+from alibi_detect.models.tensorflow.losses import elbo
+from alibi_detect.od import OutlierVAE
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from glob import glob
+from alibi_detect.utils.saving import save_detector
+
+# 이 모듈은 이상 탐지 모델을 학습하고 저장하는 역할을 수행합니다.
+
+# 이미지 파일 경로에 따라 해당 이미지들을 넘파이 배열로 변환 => RGB 형식 반환
+def img_to_np(fpaths, resize=True):  
+    img_array = []
+    for fname in fpaths:
+        try:
+            img = Image.open(fname).convert('RGB')
+            if resize: 
+                img = img.resize((64, 64))
+            img_array.append(np.asarray(img))
+        except:
+            continue
+    images = np.array(img_array)
+    return images
+
+# Data 불러오기, Split, 정규화
+Negative_data = "C:/sinheechan.github.io-master/Mlops-outlier-detection_lee/row_data/Negative/*.jpg" # 경로
+img_list = glob(Negative_data) # 불러오기 => 리스트 저장
+train_img_list, val_img_list = train_test_split(img_list, test_size=0.1, random_state=2021) # split
+x_train = img_to_np(train_img_list[:1000]) # 훈련데이터 1,000개 이미지를 넘파이로 반환
+x_train = x_train.astype(np.float32) / 255. # 이미지 정규화
+
+# Encoder 네트워크 정의
+latent_dim = 1024
+encoder_net = tf.keras.Sequential([
+    InputLayer(input_shape=(64, 64, 3)),
+    Conv2D(64, 4, strides=2, padding='same', activation=tf.nn.relu),
+    Conv2D(128, 4, strides=2, padding='same', activation=tf.nn.relu),
+    Conv2D(512, 4, strides=2, padding='same', activation=tf.nn.relu)
+])
+
+# Decoder 네트워크 정의
+decoder_net = tf.keras.Sequential([
+    InputLayer(input_shape=(latent_dim,)),
+    Dense(4 * 4 * 128),
+    Reshape(target_shape=(4, 4, 128)),
+    Conv2DTranspose(256, 4, strides=2, padding='same', activation=tf.nn.relu),
+    Conv2DTranspose(64, 4, strides=2, padding='same', activation=tf.nn.relu),
+    Conv2DTranspose(32, 4, strides=2, padding='same', activation=tf.nn.relu),
+    Conv2DTranspose(3, 4, strides=2, padding='same', activation='sigmoid')
+])
+
+# 이상 탐지 모델 훈련
+od = OutlierVAE(
+    threshold=.005, # 임계값 : 0.005 설정
+    score_type='mse', # MSE 평가지표_메뉴얼
+    encoder_net=encoder_net,
+    decoder_net=decoder_net,
+    latent_dim=latent_dim,
+)
+od.fit(x_train, epochs=10, verbose=True) # epochs = 1
+
+# 모델 저장
+save_detector(od, 'outlier_Scratch_model')
