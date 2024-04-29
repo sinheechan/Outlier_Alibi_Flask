a22298d3ace605742e1b332509964f7b6dd4af85
Sinheechan <147711497+sinheechan@users.noreply.github.com>
2024-04-29 11:46:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,43 @@
+# import matplotlib
+# matplotlib.use('TkAgg')  # 백엔드를 Agg로 설정 => Flask 서버 충돌 우려로 인한 주석처리
+# import matplotlib.pyplot as plt
+import numpy as np
+import os
+import time # 초 단위
+from datetime import datetime # 날짜/초 단위
 import matplotlib
-# matplotlib.use('Agg')  # 백엔드를 Agg로 설정 => Flask 충돌우려로 인한 주석처리
+matplotlib.use('Agg')
+
 from flask import Flask, request, render_template, jsonify
+from PIL import Image
 from alibi_detect.utils.saving import load_detector
 from alibi_detect.od import OutlierVAE
 from alibi_detect.utils.visualize import plot_instance_score, plot_feature_outlier_image
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-import os
-import time
+
+from save_results_image import save_result_image
 
 # 서버를 구축하는 모듈입니다.
 
 app = Flask(__name__)
 
 # train.py 결과 모델 로드
-od = load_detector('outlier_detection_model')
+od = load_detector('outlier_Scratch_model')
 
 # 결과를 저장할 폴더 경로
-results_folder = 'C:/sinheechan.github.io-master/Mlops-outlier-detection_lee/results_folder'
+results_folder = 'results'
 
 # result 폴더가 없는 경우 신규 폴더 생성
 if not os.path.exists(results_folder):
     os.makedirs(results_folder)
 
-# 파일로 결과를 저장
-def save_result_image(fig, filename_prefix):
-    if fig is None: # 예외처리
-        return None
-    
-    img_path = os.path.join(results_folder, f"{filename_prefix}_{int(time.time())}.png")
-    print("이미지가 저장되었습니다.:", img_path)
-    
-    fig.savefig(img_path)
-    plt.close(fig)
-    
-    return img_path
+# 파일로 결과를 저장 => save_results_image.py 모듈로 분리함
 
 # 파일을 업로드하면 분석결과 출력
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST']) # GET or POST
 
 def home():
     if request.method == 'POST':
+        from app import od
         
         # 예외처리1 => 파일을 아예 제출하지 않은 경우
         if 'file' not in request.files:
@@ -68,13 +64,43 @@ def home():
             # od.predict : 이미지가 얼마나 정상적인지를 나타내는 값 반환
             od_preds = od.predict(img, outlier_type='instance', return_feature_score=True, return_instance_score=True)
 
-            # 결과를 시각화, 시각화된 결과를 이미지 파일로 저장
+            # 시각화를 하지 않고 결과를 파일로 저장
+
+            results_outlier_image = "C:/sinheechan.github.io-master/Outlier_Alibi_MLops/results_outlier_image"
+
             instance_score_plot = plot_instance_score(od_preds, [0], ['normal', 'outlier'], od.threshold)
-            instance_score_plot_path = save_result_image(instance_score_plot, 'instance_score_plot')
+            instance_score_plot_path = save_result_image(instance_score_plot, results_outlier_image, 'instance_score_plot')
 
             feature_outlier_image = plot_feature_outlier_image(od_preds, img, X_recon=x_recon, max_instances=5, outliers_only=False)
-            feature_outlier_image_path = save_result_image(feature_outlier_image, 'feature_outlier_image')
+            feature_outlier_image_path = save_result_image(feature_outlier_image, results_outlier_image, 'feature_outlier_image')
 
-            return render_template('index.html', prediction="Anomaly Score: {:.4f}".format(od_preds['data']['instance_score'][0]), instance_score_plot=instance_score_plot_path, feature_outlier_image=feature_outlier_image_path)
-    else:
-        return render_template('index.html', prediction=None)
+            return render_template('index.html', prediction="Anomaly Score: {:.4f}".format(od_preds['data']['instance_score'][0]), 
+                                   instance_score_plot=instance_score_plot_path, feature_outlier_image=feature_outlier_image_path)
+
+# GET 요청을 처리하는 엔드포인트
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', prediction=None)
+
+# client 이미지 파일을 받아들이고, 해당 이미지를 모델에 전달하여 이상 탐지를 수행한 후 Anomaly Score를 반환
+@app.route('/predict', methods=['POST']) # 'GET' or 'POST'
+
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+                img = Image.open(file.stream).convert('RGB')
+                img = img.resize((64, 64))
+                img = np.asarray(img)
+                img = img.astype(np.float32) / 255.
+                img = img.reshape(1, 64, 64, 3)
+
+                # Prediction
+                od_preds = od.predict(img, outlier_type='instance', return_feature_score=True, return_instance_score=True)
+                instance_score = od_preds['data']['instance_score'][0]
+                return jsonify(prediction="Anomaly 점수: {:.4f}".format(instance_score))
+        
+    return jsonify(prediction=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
